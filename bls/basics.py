5fbff5d1128ceddd77c71da836d6894157014c45
Anton Shkrebtiienko <abtienko@gmail.com>
2025-03-06 02:51:49 +0000

    gitignore added variables and expressions discovered

---
+++ bls/basics.py
@@ -0,0 +1,13 @@
+pocket = 5
+coefficient = 2.85
+sting_variable = 'just a string'
+class SomeCustomClass : pass
+fruits = ['Apples', 'Bananas', 'Grapes', 'Tomatoes']
+instance = SomeCustomClass()
+# print(type(instance), type(fruits))
+
+# variables discovered
+
+# expressions
+print(2 * 3.12123 * 10)
+
