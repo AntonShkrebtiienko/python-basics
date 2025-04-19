f9622c5b337ab0f94e2d58db42e3ac09dc38d7f9
Anton Shkrebtiienko <abtienko@gmail.com>
2025-04-19 17:37:01 +0100

    some mild cleaning and changing: 1. inputs are already returning strings so unnecessary pre-define it. 2. all stings quotes are same now '' 3. changed logic for is_on_project input data processing 4. slightly improved outputs of first task

--- bls/collections.py
+++ bls/collections.py
@@ -4,18 +4,18 @@ import copy
 developer: dict = {}
 
 while True:
-    first_name: str = input('What is your first name? ').strip()
+    first_name = input('What is your first name? ').strip()
     if len(first_name) > 0:
-        developer.setdefault("first name", first_name)
+        developer.setdefault('first name', first_name)
         break
     else:
         print('Input your name, please :)')
     continue
 
 while True:
-    last_name: str = input('What is your last name? ').strip()
+    last_name = input('What is your last name? ').strip()
     if len(last_name) > 0:
-        developer.setdefault("last name", last_name)
+        developer.setdefault('last name', last_name)
         break
     else:
         print('Input your last name, please :)')
@@ -25,7 +25,7 @@ while True:
     work_experience = input('What is your work experience in years? ')
     try:
         work_experience = int(work_experience)
-        developer.setdefault("work experience", work_experience)
+        developer.setdefault('work experience', work_experience)
         break
     except ValueError:
         if not work_experience:
@@ -38,7 +38,7 @@ while True:
     age = input('What is your age? ')
     try:
         age = int(age)
-        developer.setdefault("age", age)
+        developer.setdefault('age', age)
         break
     except ValueError:
         if not age:
@@ -52,32 +52,45 @@ while True:
     try:
         salary_converted = int(salary_input) / 1000
         salary = round(salary_converted, 3)
-        developer.setdefault("salary", salary_converted)
+        developer.setdefault('salary', salary_converted)
         break
     except ValueError:
-        print('error')
+        print('input an integer value of your salary')
     continue
 
 while True:
     try:
-        is_on_project = input('Are u on project? (yes/no):').strip().lower() == 'yes'
-        developer.setdefault("is on project", is_on_project)
+        is_on_project = input('Are u on project? (yes/no):').strip().lower()
+
+        match is_on_project:
+            case 'yes':
+                developer.setdefault('is on project', 'Working on project')
+            case 'no':
+                developer.setdefault('is on project', 'Open to work')
+            case _:
+                print('Please answer yes or no')
+                continue
+
         break
     except ValueError:
         print('error')
-    continue
+        continue
 
 
-print(
-    developer["first name"] + " | " +
-    developer["last name"] + " | " +
-    str(developer["work experience"]) + " | " +
-    str(developer["age"]) + " | " +
-    str(developer["salary"]) + " | " +
-    str(developer["is on project"])
+print('First name: ' + developer['first name'] + ' |'  +
+    ' Last name: ' +
+    developer['last name'] + ' |'  +
+    ' Work experience: ' +
+    str(developer['work experience']) + ' |'  +
+    ' Age: ' +
+    str(developer['age']) + ' |'  +
+    ' Salary: ' +
+    str(developer['salary']) + ' |'  +
+    ' On project?(True/False) :' +
+    str(developer['is on project'])
 )
 print(
-    "{} | {} | Experience: {} yrs | Age: {} | Salary: ${:.3f} | On project: {}".format(
+    'First name: {} | Last name: {} | Experience: {} yrs | Age: {} | Salary: ${:.3f} | On project: {}'.format(
         developer['first name'],
         developer['last name'],
         developer['work experience'],
@@ -87,35 +100,35 @@ print(
     )
 )
 print(
-    f"{developer['first name']} | {developer['last name']} | "
-    f"Experience: {developer['work experience']} yrs | "
-    f"Age: {developer['age']} | "
-    f"Salary: ${developer['salary']} | "
-    f"On project: {developer['is on project']}"
+    f'First name: {developer['first name']} | Last name: {developer['last name']} | '
+    f'Experience: {developer['work experience']} yrs | '
+    f'Age: {developer['age']} | '
+    f'Salary: ${developer['salary']} | '
+    f'On project: {developer['is on project']}'
 )
 
 for key, value in developer.items():
-    print(f"key={key.upper()}; value=<{value}>")
+    print(f'key={key.upper()}; value=<{value}>')
 
 # Task 2
-projects = [f"Project{i}" for i in range(1,6,1)]
-developer.setdefault("projects", projects)
+projects = [f'Project{i}' for i in range(1,6,1)]
+developer.setdefault('projects', projects)
 
-for i in range(len(developer["projects"])) :
+for i in range(len(developer['projects'])) :
     if i % 2 == 0 and i > 0:
-        print(f"Index={i}; Project={developer["projects"][i]}")
+        print(f'Index={i}; Project={developer['projects'][i]}')
 
-for i, x in enumerate(developer["projects"]):
+for i, x in enumerate(developer['projects']):
     if i % 2 == 0 and i > 0:
-        print(f"Index={i}; Project={developer["projects"][i]}")
+        print(f'Index={i}; Project={developer['projects'][i]}')
 
 # Task 3
 developer_copy = copy.deepcopy(developer)
-developer_copy["projects"].append("Project6")
-developer["projects"].pop(0)
-developer["projects"] = set(developer["projects"])
-developer_copy["projects"] = set(developer_copy["projects"])
+developer_copy['projects'].append('Project6')
+developer['projects'].pop(0)
+developer['projects'] = set(developer['projects'])
+developer_copy['projects'] = set(developer_copy['projects'])
 
-print("Union:", developer["projects"] | developer_copy["projects"])
-print("Intersection:", developer["projects"] & developer_copy["projects"])
-print("New dev difference:", developer_copy["projects"] - developer["projects"])
+print('Union:', developer['projects'] | developer_copy['projects'])
+print('Intersection:', developer['projects'] & developer_copy['projects'])
+print('New dev difference:', developer_copy['projects'] - developer['projects'])
